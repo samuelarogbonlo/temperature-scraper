a69944e18630c29fa1ec16a67e0cf12edd226359
samuelarogbonlo <sbayo971@gmail.com>
2024-02-02 00:25:11 +0100

    chore: log error when IPFS_GATEWAY_URL not available

--- ipfs-upload/main.go
+++ ipfs-upload/main.go
@@ -129,6 +129,9 @@ func main() {
     }
 
 	ipfsGatewayUrl := os.Getenv("IPFS_GATEWAY_URL")
+	if ipfsGatewayUrl == ""{
+		log.Fatal("IPFS GATEWAY URL not set in environment variable IPFS_GATEWAY_URL")
+	}
     fmt.Printf("File uploaded to IPFS with CID: %s\n", cid)
     fmt.Printf("Publicly accessible link (via IPFS gateway): %s/ipfs/%s\n", ipfsGatewayUrl, cid)
 }
